131208ba6185ef10cbcaad2064b11ef6587f79da
Imperiouspi <nitrogen@cassidys.ca>
2014-01-18 23:42:18 -0500

    More GUI

---
+++ src/gui/ApocalypsePane.java
@@ -0,0 +1,16 @@
+package gui;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ApocalypsePane extends JPanel{
+	public ApocalypsePane(){
+		super();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		//TODO apocalypse picture
+	}
+}
