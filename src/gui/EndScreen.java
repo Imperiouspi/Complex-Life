25db49366e2ea10d2954e6793e8943db3826930c
Helen-Saire <miranda.jy.zhou@gmail.com>
2014-01-22 11:48:58 -0500

    minor cosmetic change to EndScreen

--- src/gui/EndScreen.java
+++ src/gui/EndScreen.java
@@ -9,12 +9,12 @@ import javax.swing.JTextArea;
 public class EndScreen extends JFrame {
  public EndScreen(int score){
  	super("END");
- 	JTextArea txtArea = new JTextArea("Your Score: " + score);
+ 	JTextArea txtArea = new JTextArea("Sorry, You Lose.\nYour Score: " + score);
  	txtArea.setFont(new Font("Papyrus", Font.PLAIN, 20));
  	txtArea.setEditable(false);
  	add(txtArea, BorderLayout.CENTER);
  	setDefaultCloseOperation(EXIT_ON_CLOSE);
- 	setSize(200, 80);
+ 	setSize(200, 100);
  	setLocationRelativeTo(null);
  	setVisible(true);
  }
