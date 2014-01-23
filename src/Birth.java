7aaf854850a8a4d382a9fc031315eda74554f001
Helen-Saire <miranda.jy.zhou@gmail.com>
2014-01-23 00:49:17 -0500

    Minor changes to the help window

--- src/Birth.java
+++ src/Birth.java
@@ -18,13 +18,14 @@ public class Birth {
 		window.setVisible(true);
 		JFrame Help = new JFrame();
 		JTextArea helpText = new JTextArea("How to play:"
-			+"\n-select an animal from the combo box."
+			+"\n-Select an animal from the combo box."
 			+"\n-Adjust the RGB sliders to change the color of the species."
 			+"\n-The chance of breeding is the percent chance that an animal will breed."
-			+"\n- The cool down is how long the animal has to wait before breeding again."
+			+"\n- The countdown is how long the animal has to wait before breeding again."
 			+"\n- The longer the delay slider is set for, the larger the delay between movements"
 			+"\n- Press apocalypse to kill everything except one animal"
-			+"\n- At the bottom is displayed the number of animals left, and some news based on what is happening in the world.");
+			+"\n- Click on an animal in the world to see its icon and statistics on the right"
+			+"\n- At the bottom is displayed the number of animals left, and some news based on\n   what is happening in the world.");
 		helpText.setEditable(false);
 		Help.add(helpText);
 		Help.setSize(new Dimension(500, 200));
